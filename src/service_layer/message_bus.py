ca908277393aeab431ead878d2913d19a6f44851
Denis-Frunza <46573296+Denis-Frunza@users.noreply.github.com>
2024-02-13 07:47:14 +0200

    refactoring (#3)

--- src/service_layer/message_bus.py
+++ src/service_layer/message_bus.py
@@ -1,27 +1,73 @@
-from typing import Callable, Dict, List, Type
+import logging
+
+from typing import Callable, Dict, List, Type, Union
 
 from src.domain import events
 from src.service_layer import handlers
 from src.service_layer.unit_of_work import UnitOfWork
+from src.domain import commands
+
+
+logger = logging.getLogger(__name__)
+
+Message = Union[commands.Command, events.Event]
 
 
 def handle(
-    event: events.Event,
+    message: Message,
     uow: UnitOfWork,
 ):
     results = []
-    queue = [event]
+    queue = [message]
     while queue:
-        event = queue.pop(0)
-        for handler in HANDLERS[type(event)]:
-            results.append(handler(event, uow=uow))
-            queue.extend(uow.collect_new_events())
+        message = queue.pop(0)
+        if isinstance(message, events.Event):
+            handle_event(message, queue, uow)
+        elif isinstance(message, commands.Command):
+            cmd_result = handle_command(message, queue, uow)
+            results.append(cmd_result)
+        else:
+            raise Exception(f"{message} was not an Event or Command")
     return results
 
 
-HANDLERS: Dict[Type[events.Event], List[Callable]] = {
-    events.BatchCreated: [handlers.add_batch],
-    events.BatchQuantityChanged: [handlers.change_batch_quantity],
-    events.AllocationRequired: [handlers.allocate],
+
+def handle_event(
+    event: events.Event,
+    queue: List[Message],
+    uow: UnitOfWork,
+):
+    for handler in EVENT_HANDLERS[type(event)]:
+        try:
+            logger.debug("handling event %s with handler %s", event, handler)
+            handler(event, uow=uow)
+            queue.extend(uow.collect_new_events())
+        except Exception:
+            logger.exception("Exception handling event %s", event)
+            continue
+
+def handle_command(
+    command: commands.Command,
+    queue: List[Message],
+    uow: UnitOfWork,
+):
+    logger.debug("handling command %s", command)
+    try:
+        handler = COMMAND_HANDLERS[type(command)]
+        result = handler(command, uow=uow)
+        queue.extend(uow.collect_new_events())
+        return result
+    except Exception:
+        logger.exception("Exception handling command %s", command)
+        raise
+
+
+EVENT_HANDLERS: Dict[Type[events.Event], List[Callable]] = {
     events.OutOfStock: [handlers.send_out_of_stock_notification],
+}
+
+COMMAND_HANDLERS: Dict[Type[commands.Command], List[Callable]] = {
+    commands.Allocate: handlers.allocate,
+    commands.CreateBatch: handlers.add_batch,
+    commands.ChangeBatchQuantity: handlers.change_batch_quantity,
 }
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,15 @@
+# pylint: disable=redefined-outer-name
+
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, clear_mappers
-
+import time
+from pathlib import Path
+import requests
+from sqlalchemy.exc import OperationalError
 
 from src.adapters.orm import metadata, start_mappers
+from src import config
 
 
 @pytest.fixture
@@ -22,3 +28,33 @@ def mappers():
     start_mappers()
     yield
     clear_mappers()
+
+
+def wait_for_postgres_to_come_up(engine):
+    deadline = time.time() + 10
+    while time.time() < deadline:
+        try:
+            return engine.connect()
+        except OperationalError:
+            time.sleep(0.5)
+    pytest.fail("Postgres never came up")
+
+
+@pytest.fixture(scope="session")
+def postgres_db():
+    engine = create_engine(config.get_postgres_uri())
+    wait_for_postgres_to_come_up(engine)
+    metadata.create_all(engine)
+    return engine
+
+
+@pytest.fixture
+def postgres_session_factory(postgres_db):
+    start_mappers()
+    yield sessionmaker(bind=postgres_db)
+    clear_mappers()
+
+
+@pytest.fixture
+def postgres_session(postgres_session_factory):
+    return postgres_session_factory()
